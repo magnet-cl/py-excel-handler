4cd1a648a89e97faf11d7c76c006dc38702fe201
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-07-04 18:53:51 -0400

    Included xlutils into the requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from distutils.core import setup
-setup(name='py-excel-handler',
-      version='0.1',
-      py_modules=['excel_handler'],
-      )
+setup(
+    name='py-excel-handler',
+    version='0.1',
+    py_modules=['excel_handler'],
+    install_requires=[
+        'xlutils >= 1.6.0',
+    ],
+)
