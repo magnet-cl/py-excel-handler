e8b6d33a8a41853db0d902803803290c089fdcb1
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-08-29 12:21:20 -0400

    New Setup.py to help with heroku

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-try:
-    from setuptools import setup
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+#
+from setuptools import setup
 
 setup(
     name='py-excel-handler',
